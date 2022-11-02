b81e465b4fd02d22ee5094a4ae41b4d33ae0ee25
Melvin Batista <mbatista@infrasystpr.com>
2022-11-01 23:48:13 -0400

    template refs - comp api

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,35 @@
 <template>
   <div class="home">
     <h1>home</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
+    <p ref="par">My name is {{ name }} and my age is {{ age }}</p>
     <button @click="handleClick">click me</button>
   </div>
 </template>
 
 <script>
+import { ref } from '@vue/reactivity';
 export default {
   name: "Home",
   setup() {
-    console.log("setup");
+    console.log(this);
+
+    const par = ref('hello')
+    console.log(par, par.value)
 
     let name = "mario";
     let age = 30;
 
     const handleClick = () => {
-      console.log("you clicked me");
+      console.log(par, par.value);
+      par.value.classList.add("test")
+      par.value.textContent = "Hello, ninjas."
     };
 
     return {
       name,
       age,
       handleClick,
+      par
     };
   },
 };
