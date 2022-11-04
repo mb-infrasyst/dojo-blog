3101f0c2db1930d31cc854656e7eccc2ebd45593
Melvin Batista <mbatista@infrasystpr.com>
2022-11-03 22:21:54 -0400

    ref vs reactive

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,38 +1,35 @@
 <template>
   <div class="home">
-    <h1>home</h1>
-    <p>My name is {{ name }} and my age is {{ age }}</p>
-    <button @click="handleClick">click me</button>
-    <button @click="age++">add 1 to age</button>
-    <input type="text" v-model="name">
+    <h1>Home</h1>
+
+    <h2>Refs</h2>
+    <p>{{ ninjaOne.name }} - {{ ninjaOne.age }}</p>
+    <button @click="updateNinjaOne">Update ninja one</button>
+
+    <h2>Reactive</h2>
+    <p>{{ ninjaTwo.name }} - {{ ninjaTwo.age }}</p>
+    <button @click="updateNinjaTwo">Update ninja two</button>
+
   </div>
 </template>
 
 <script>
-import { ref } from "@vue/reactivity";
+import { ref, reactive } from "@vue/reactivity";
 export default {
   name: "Home",
   setup() {
-    // const p = ref(null);
+    const ninjaOne = ref({ name: "mario", age: 30 });
+    const ninjaTwo = reactive({ name: "luigi", age: 35 });
 
-    // let text = "Hello World!";
-    // let result = text.toUpperCase();
-
-    const name = ref("mario");
-    const age = ref(30);
-    // const textallcapsout = ref("hello melvin, the man!")
-
-    const handleClick = () => {
-      name.value = "luigi";
-      age.value = 35;
-      // textallcapsout.value = textallcapsout.value.toUpperCase()
+    const updateNinjaOne = () => {
+      ninjaOne.value.age = 45;
     };
 
-    return {
-      name,
-      age,
-      handleClick,
+    const updateNinjaTwo = () => {
+      ninjaTwo.age = 40;
     };
+
+    return { ninjaOne, updateNinjaOne, ninjaTwo, updateNinjaTwo };
   },
 };
 </script>
