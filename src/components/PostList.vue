fff01384d7cce5ca359eca65a54c56419baa79ba
Melvin Batista <mbatista@infrasystpr.com>
2022-11-06 18:11:29 -0400

    fetching data in setup

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -13,19 +13,9 @@ export default {
   props: ["posts"],
   components: { SinglePost },
   setup(props) {
-    onMounted(() => {
-      console.log("component mounted");
-    });
-    onUnmounted(() => {
-      console.log("component unmounted");
-    });
-    onUpdated(() => {
-      console.log("component updated");
-    });
+
   },
-  mounted() {
-    console.log("mounted using options api");
-  },
+
 };
 </script>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,28 +1,39 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <PostList v-if="showPosts" :posts="posts"/>
-    <button @click="showPosts = !showPosts">toogle posts</button>
-    <button @click="posts.pop()">delete a post</button>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length"><PostList :posts="posts" /></div>
+    <div v-else>Loading...</div>
   </div>
 </template>
 
 <script>
 import PostList from "../components/PostList.vue";
 import { ref } from "@vue/reactivity";
-import { computed, watch, watchEffect } from "@vue/runtime-core";
 export default {
   name: "Home",
   components: { PostList },
   setup() {
-    const posts = ref([
-      { title: "welcome to the blog", body: "Quisque imperdiet eget nunc at consequat. Praesent euismod a magna eget efficitur. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Sed tincidunt vehicula sollicitudin. Quisque nec porttitor leo. Donec maximus, turpis tincidunt cursus ornare, neque mi iaculis velit, ut bibendum massa odio nec erat. Aenean sed urna ante. Donec fringilla, mauris eu sagittis rutrum, ipsum mi fermentum turpis, ut consequat est ipsum quis sem. Ut hendrerit elit vitae ante dignissim aliquam. Maecenas varius tempor scelerisque. Sed sodales dignissim lobortis. In tristique, est id iaculis congue, sem nisi porta felis, blandit pulvinar enim erat ultricies justo. Mauris vulputate leo lorem, at lobortis nibh cursus non. Cras id quam varius, vestibulum libero in, tincidunt lorem.", id: 1 },
-      { title: "top 5 CSS tips", body: "lorem ipsum", id: 2 },
-    ]);
+    const posts = ref([]);
+    const error = ref(null);
 
-    const showPosts = ref(true)
+    const load = async () => {
+      try {
+        let data = await fetch("http://localhost:3000/posts");
+        if (!data.ok) {
+          throw Error("no data avialable");
+        }
+        posts.value = await data.json();
+      } catch (err) {
+        error.value = err.message;
+        console.log(error.value);
+      }
+    };
+    load();
 
-    return { posts, showPosts };
+    const showPosts = ref(true);
+
+    return { posts, error };
   },
 };
 </script>
